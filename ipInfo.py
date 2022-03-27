c0af916da6d1c96965118c6adc3d96e370d09df5
Sobol Roman <57962352+TsuniQwer@users.noreply.github.com>
2022-03-27 10:03:54 +0300

    Update ipInfo.py

--- ipInfo.py
+++ ipInfo.py
@@ -19,15 +19,20 @@ print("""
 
 """)
 
-
+from ip2geotools.databases.noncommercial import DbIpCity
 
 try:
 
-  from ip2geotools.databases.noncommercial import DbIpCity
   ip = input("IP: ")
   response = DbIpCity.get(ip, api_key='free')
   print("Country: " + str(response.country))
   print("Region: " + str(response.region))
+Delivery-food Public
+
+HTML
+
+1
+ipInfo Public
   print("City: " + str(response.city))
   print("\nlatitude: "+str(response.latitude) + " longitude: "+str(response.longitude))
   
