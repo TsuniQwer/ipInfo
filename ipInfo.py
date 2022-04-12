25016f253b4b26509c4c898685391800fc0eed92
Sobol Roman <57962352+TsuniQwer@users.noreply.github.com>
2022-04-13 01:19:17 +0300

    Update ipInfo.py

--- ipInfo.py
+++ ipInfo.py
@@ -27,12 +27,6 @@ try:
   response = DbIpCity.get(ip, api_key='free')
   print("Country: " + str(response.country))
   print("Region: " + str(response.region))
-Delivery-food Public
-
-HTML
-
-1
-ipInfo Public
   print("City: " + str(response.city))
   print("\nlatitude: "+str(response.latitude) + " longitude: "+str(response.longitude))
   
